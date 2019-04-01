11b7b80dcb0f0c1e263417c2d61f1ff0e061a5b3
Andre Tonelli <aveigas@br.ibm.com>
2019-04-01 08:59:14 -0300

    Create Model for Tasks database
    it is using db TaskList for SQLAlchemy and Marshmallow Schema TaskListSchema

---
+++ models.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from config import db, ma
+
+class TaskList(db.Model):
+    __tablename__ = 'task'
+    task_id = db.Column(db.Integer, primary_key=True)
+    tgroup = db.Column(db.String(32))
+    tname = db.Column(db.String(128), index=True)
+    tdate = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    done_flag = db.Column(db.String(1))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class TaskListSchema(ma.ModelSchema):
+    class Meta:
+        model = TaskList
+        sqla_session = db.session   
